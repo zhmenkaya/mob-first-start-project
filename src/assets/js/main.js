d27c4d3bd1204fe08f4bbe8fd2080773d4f50b83
Klimovich Yauheniya <zh.shuravina@gmail.com>
2021-09-29 17:57:58 +0300

    slider unclick in js commit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,40 @@
-// $(".slider").slick({
-//     slidesToShow: 1,
-//     dots: true,
-//     arrows: true
-// });
-
 $(document).ready(function(){
     $('.slider').slick({
         slidesToShow: 1,
         arrow:true,
-        dots:true
+        dots:true,
+        responsive:[
+            {
+                breakpoint:1280,
+                settings:"unlick"
+            },
+            {
+                breakpoint:768,
+                settings:{
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrow:true,
+                    dots:true,
+                }
+            },
+            {
+                breakpoint:576,
+                settings:{
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrow:true,
+                    dots:true,
+                }
+            },
+            {
+                breakpoint:320,
+                settings:{
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrow:true,
+                    dots:true,
+                }
+            }
+        ]
     });
-  });
+});
