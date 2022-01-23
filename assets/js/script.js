70c5d6b602948937ed510b776169c6aa10202e8a
Klimovich Yauheniya <zh.shuravina@gmail.com>
2022-01-23 12:59:24 +0300

    Initial dist subtree commit

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+
+const slideSrettings = {
+    slidesToShow: 1,
+    arrow:true,
+    dots:true,
+    responsive:[
+        {
+            breakpoint:768,
+            settings:{
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrow:true,
+                dots:true,
+            }
+        },
+        {
+            breakpoint:576,
+            settings:{
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrow:true,
+                dots:true,
+            }
+        },
+        {
+            breakpoint:320,
+            settings:{
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrow:true,
+                dots:true,
+            }
+        }
+    ]
+}
+
+$(window).on("load resize", function() {
+    const width = $(document).width();
+    
+    if (width >= 1280) {
+        $('.slider').slick('unslick');
+    } else {
+        $('.slider').not('.slick-initialized').slick(slideSrettings);
+    }
+});
+
+
+
+//# sourceMappingURL=script.js.map
